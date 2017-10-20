fd22c6db9b81d798a6db53eaa9a0c5b92362b2b0
Tony Deng <wolf.deng@gmail.com>
2017-10-20 10:59:26 +0800

    fix(1.0): 修复entrypont.sh脚本中生成database的sql错误
    Signed-off-by: Tony Deng <wolf.deng@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # docker entrypoint script
-# configures and start MySQL
+# configures and starts MySQL
 
 echo "[i] Start MySQL with config /etc/mysql/my.cnf"
 
@@ -17,11 +17,11 @@ else
 
     mkdir -p /mysql/data/mysql
 
-    mysql_install_db --user=root >> /dev/null
+    mysql_install_db --user=root > /dev/null
 
     if [ "$MYSQL_ROOT_PASSWORD" = "" ]; then
         MYSQL_ROOT_PASSWORD=`pwgen 16 1`
-        echo "[i] MySQL root password: $MYSQL_ROOT_PASSWORD"
+        echo "[i] MySQL root Password: $MYSQL_ROOT_PASSWORD"
         echo $MYSQL_ROOT_PASSWORD > /mysql/conf/mysql/root_password
         echo $MYSQL_ROOT_PASSWORD > /mysql/data/mysql/root_password
     fi
@@ -31,22 +31,21 @@ else
     MYSQL_PASSWORD=${MYSQL_PASSWORD:-""}
 
     tfile=`mktemp`
-
     if [ ! -f "$tfile" ]; then
         return 1
     fi
 
-    cat << EOF >> $tfile
+    cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY "$MYSQL_ROOT_PASSWORD" WITH GRANT OPTION;
-GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost'  WITH GRANT OPTION;
-UPDATE user SET password=PASSWORD("") WHERE user='root' and host='localhost';
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+UPDATE user SET password=PASSWORD("") WHERE user='root' AND host='localhost';
 EOF
 
-    if [ "$MYSQL_DATABASE" != ""]; then
+    if [ "$MYSQL_DATABASE" != "" ]; then
         echo "[i] Creating database: $MYSQL_DATABASE"
-        echo "CREATE DATABSE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
+        echo "CREATE DATABASE IF NOT EXISTS \`$MYSQL_DATABASE\` CHARACTER SET utf8 COLLATE utf8_general_ci;" >> $tfile
 
         if [ "$MYSQL_USER" != "" ]; then
             echo "[i] Creating user: $MYSQL_USER with password $MYSQL_PASSWORD"
